9b48a3e8b79c76a55594e5602810b08684e1f0f8
Shikhar Vashistha <masteroftheirclassic@gmail.com>
2021-07-28 11:41:39 +0000

    Added sample build dir

---
+++ src/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /workspace/shell/src
+# Build directory: /workspace/shell/src/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[test]=] "a")
+set_tests_properties([=[test]=] PROPERTIES  _BACKTRACE_TRIPLES "/workspace/shell/src/CMakeLists.txt;9;add_test;/workspace/shell/src/CMakeLists.txt;0;")
