6823876704fa5a1370da85ef3b5fbb0b67755083
Shikhar Vashistha <masteroftheirclassic@gmail.com>
2021-07-28 11:42:42 +0000

    Fixed CI

--- src/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/shell/src
-# Build directory: /workspace/shell/src/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[test]=] "a")
-set_tests_properties([=[test]=] PROPERTIES  _BACKTRACE_TRIPLES "/workspace/shell/src/CMakeLists.txt;9;add_test;/workspace/shell/src/CMakeLists.txt;0;")
